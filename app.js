63cb212d01c136a0e192300840834cb4cbd3cdf5
Pawan072 <py8795116@gmail.com>
2025-10-17 07:50:14 +0530

    Error improvement on client site

--- app.js
+++ app.js
@@ -32,18 +32,18 @@ app.get("/",(req, res)=>{
 })
 
 //index route 
-app.get("/listings",async(req, res)=>{
+app.get("/listings",wrapAsync (async(req, res)=>{
     const allListing = await Listing.find({});
     res.render("listings/index.ejs", {allListing});
-})
+}))
 
 // show route
 
-app.get("/listing/:id", async (req, res)=>{
+app.get("/listing/:id", wrapAsync (async (req, res)=>{
     let{id}= req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-})
+}))
 
 
 //new route
@@ -54,6 +54,9 @@ app.get("/listings/new", (req, res)=>{ // square brackets replace by curly brack
 
 //create Route
 app.post("/listings", wrapAsync (async (req, res, next)=>{
+    if(!req.body.listing){
+        throw new ExpressError();
+    }
     let listings = req.body.listings;
     const newlisting = new Listing (listings);
     await newlisting.save();
@@ -61,27 +64,30 @@ app.post("/listings", wrapAsync (async (req, res, next)=>{
 }))
 
 // edit route 
-app.get("/listing/:id/edit", async (req, res)=>{
+app.get("/listing/:id/edit", wrapAsync (async (req, res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", {listing});
-})
+}))
 
 //update route
-app.put("/listing/:id", async (req, res)=>{
+app.put("/listing/:id", wrapAsync (async (req, res)=>{
+    if(!req.params.listing){
+        throw new ExpressError(400, "Send some valid data for listing");
+    }
     let{id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listing/${id}`);
-})
+}))
 
 
 //delete route 
-app.delete("/listing/listings/:id", async (req, res)=>{
+app.delete("/listing/listings/:id",wrapAsync (async (req, res)=>{
     let{id}= req.params;
     let deltedListing = await Listing.findByIdAndDelete(id);
     console.log(deltedListing);
     res.redirect("/listings");
-})
+}))
 
 // app.get("/testListing",async (req, res)=>{
 //     let sampleListing = new listing({
@@ -101,8 +107,8 @@ app.all("/*splat",(req, res, next)=>{
 
 
 app.use((err, req, res, next)=>{
-    let{statusCode, message}= err;
-    res.status(statusCode).send(message);
+    let{statusCode=500, message="something went wrong!"}= err;
+    res.render("Error.ejs", {err});
 });
 
 
